9dcceb4d0277a5b96e71269e4e4a70d756ba4438
Howard <howardguoui@gmail.com>
2022-03-10 00:26:37 -0500

    update first instance

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-// Code goes here!
+class ProjectInput {
+    templateElem : HTMLTemplateElement;
+    hostElem : HTMLDivElement;
+    element : HTMLFormElement;
+    constructor() {
+        // const templateEl = document.getElementById('project-inpiut');
+        // if(templateEl) {
+        //     this.templateElem = this.templateEl;
+        // }
+        this.templateElem = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElem = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElem.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+    private attach() {
+        this.hostElem.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+
+const newInput = new ProjectInput();
