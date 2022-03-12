a47460e06f2829e2a581bebe2fdd93838c2f1783
Howard <howardguoui@gmail.com>
2022-03-12 17:21:29 -0500

    update input

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+function autobind(_: any, _2 : string, descriptor: PropertyDescriptor) {
+    const originMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originMethod.bind(this);
+            return boundFn
+        }
+    }
+    return adjDescriptor;
+}
+
 class ProjectInput {
     templateElem : HTMLTemplateElement;
     hostElem : HTMLDivElement;
     element : HTMLFormElement;
+    titleElem : HTMLInputElement;
+    descriptionElem : HTMLInputElement;
+    peopleElem : HTMLInputElement;
+
     constructor() {
         // const templateEl = document.getElementById('project-inpiut');
         // if(templateEl) {
@@ -12,10 +28,51 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElem.content, true);
         this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+        this.titleElem = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionElem = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleElem = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure();
         this.attach();
     }
     private attach() {
         this.hostElem.insertAdjacentElement('afterbegin', this.element);
+    }
+    private clear() {
+        this.titleElem.value = '',
+        this.descriptionElem.value = '',
+        this.peopleElem.value = ''
+
+    }
+    private gatherUserInput(): [string,string, number] | void{
+        const enterTitle = this.titleElem.value;
+        const enterDescriptionElem = this.descriptionElem.value;
+        const enterPeopleElem = this.peopleElem.value;
+        if(enterTitle.trim().length === 0 || enterDescriptionElem.trim().length === 0|| enterPeopleElem.trim().length === 0) {
+            alert('wrong')
+            return;
+        } else {
+            return [enterTitle, enterDescriptionElem, +enterPeopleElem];
+        }
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        console.log("test -->",this.titleElem.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, people] = userInput
+            console.log("title - desc - people", title, desc, people)
+            this.clear();
+        }
+    }
+    // private configure() {
+    //     this.element.addEventListener('submit', this.submitHandler.bind(this))
+    // }
+        private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 }
 
