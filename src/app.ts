b5f0222ebc7c30ae61485240dc44d41848661249
Howard <howardguoui@gmail.com>
2022-03-15 23:46:51 -0400

    update project item class

--- src/app.ts
+++ src/app.ts
@@ -166,6 +166,28 @@ class ProjectList2 {
         this.hostElem.insertAdjacentElement('beforeend', this.element);
     }
 }
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+  
+    constructor(hostId: string, project: Project) {
+      super('single-project', hostId, false, project.id);
+      this.project = project;
+  
+      this.configure();
+      this.renderContent();
+    }
+  
+    configure() {}
+  
+    renderContent() {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector(
+        'h3'
+      )!.textContent = this.project.people.toString();
+      this.element.querySelector('p')!.textContent = this.project.description;
+    }
+  }
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
 
@@ -194,13 +216,13 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         this.element.querySelector('ul')!.id = listId;
         this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
     }
-    renderProjects() {
-        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
-        listEl.innerHTML= '';
+    private renderProjects() {
+        const listEl = document.getElementById(
+          `${this.type}-projects-list`
+        )! as HTMLUListElement;
+        listEl.innerHTML = '';
         for (const prjItem of this.assignedProjects) {
-          const listItem = document.createElement('li');
-          listItem.textContent = prjItem.title;
-          listEl.appendChild(listItem)
+          new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
         }
       }
 
